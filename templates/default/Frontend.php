47a823dac7990d67a1fe86400a97696519a0d3cf
Brett Bieber <brett.bieber@gmail.com>
2006-07-11 13:46:08 +0000

    Default template is now a php UNL Template... removed un-necessary code in the css and images.

--- templates/default/Frontend.php
+++ templates/default/Frontend.php
@@ -1,38 +1,99 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" ><!-- InstanceBegin template="/Templates/php.fixed.dwt.php" codeOutsideHTMLIsLocked="false" -->
 <head>
-<link rel="stylesheet" type="text/css" media="screen" href="templates/@TEMPLATE@/main.css" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<!-- InstanceBeginEditable name="doctitle" -->
 <title><?php echo $this->doctitle; ?></title>
-</head>
-<body>
+<!-- InstanceEndEditable -->
+<link rel="stylesheet" type="text/css" media="screen" href="/ucomm/templatedependents/templatecss/layouts/main.css" />
+<link rel="stylesheet" type="text/css" media="print" href="/ucomm/templatedependents/templatecss/layouts/print.css"/>
+<script type="text/javascript" src="/ucomm/templatedependents/templatesharedcode/scripts/all_compressed.js"></script>
 
-<div id="header"></div><!-- close header -->
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/ucomm/templatedependents/templatesharedcode/includes/browsersniffers/ie.html'); ?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/ucomm/templatedependents/templatesharedcode/includes/comments/developersnote.html'); ?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/ucomm/templatedependents/templatesharedcode/includes/metanfavico/metanfavico.html'); ?>
+<!-- InstanceBeginEditable name="head" -->
+<link rel="stylesheet" type="text/css" media="screen" href="templates/@TEMPLATE@/main.css" />
+<!-- InstanceEndEditable -->
+</head>
+<body id="fixed">
+<!-- InstanceBeginEditable name="siteheader" -->
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/ucomm/templatedependents/templatesharedcode/includes/siteheader/siteheader.shtml'); ?>
+<!-- InstanceEndEditable -->
+<div id="red-header">
+	<div class="clear">
+		<h1>University of Nebraska&ndash;Lincoln</h1>
+		<div id="breadcrumbs"> <!-- InstanceBeginEditable name="breadcrumbs" -->
+			<!-- WDN: see glossary item 'breadcrumbs' -->
+			<ul>
+				<li class="first"><a href="http://www.unl.edu/">UNL</a></li>
+				<li>Events</li>
+			</ul>
+			<!-- InstanceEndEditable --> </div>
+	</div>
+</div>
+<!-- close red-header -->
+  
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/ucomm/templatedependents/templatesharedcode/includes/shelf/shelf.shtml'); ?>
 
 <div id="container">
+	<div class="clear">
+		<div id="title"> <!-- InstanceBeginEditable name="collegenavigationlist" --> <!-- InstanceEndEditable -->
+			<div id="titlegraphic">
+				<!-- WDN: see glossary item 'title graphics' -->
+				<!-- InstanceBeginEditable name="titlegraphic" -->
+				<h1>UNL's Event Publishing System</h1>
+				<h2>Plan. Publish. Share.</h2>
+				<!-- InstanceEndEditable --></div>
+			<!-- maintitle -->
+		</div>
+		<!-- close title -->
+		
+		<div id="navigation">
+			<h4 id="sec_nav">Navigation</h4>
+			<!-- InstanceBeginEditable name="navcontent" -->
+			<div id="navlinks">
+				<?php echo $this->navigation; ?>
+			</div>
+			<!-- InstanceEndEditable -->
+			<div id="nav_end"></div>
+			<!-- InstanceBeginEditable name="leftRandomPromo" -->
+			<!-- InstanceEndEditable -->
+			<!-- WDN: see glossary item 'sidebar links' -->
+			<div id="leftcollinks"> <!-- InstanceBeginEditable name="leftcollinks" -->
+				<h3>Related Links</h3>
+				<ul>
+					<li><a href="<?php echo $this->manager_uri; ?>">Event Publishing Manager</a></li>
+				</ul>
+				<!-- InstanceEndEditable --> </div>
+		</div>
+		<!-- close navigation -->
+		
+		<div id="main_right" class="mainwrapper">
+			<!--THIS IS THE MAIN CONTENT AREA; WDN: see glossary item 'main content area' -->
+			
+			<div id="maincontent"> <!-- InstanceBeginEditable name="maincontent" -->
+				<div class="two_col left"><?php UNL_UCBCN::displayRegion($this->output); ?></div>
+				<div class="col right">
+					<?php UNL_UCBCN::displayRegion($this->right); ?>
+				</div>
+				<!-- InstanceEndEditable --> </div>
+			 </div>
+		<!-- close main right -->
+	</div>
+</div>
+<!-- close container -->
 
-<div id="maintitle">
-<h1>UNL's Event Publishing System</h1>
-<h2>Plan. Publish. Share.</h2>
-</div><!-- maintitle -->
-	
-<div id="main_left">
+<div id="footer">
+	<div id="footer_floater"> <!-- InstanceBeginEditable name="optionalfooter" --> <!-- InstanceEndEditable -->
+		<div id="copyright"> <!-- InstanceBeginEditable name="footercontent" -->
+			
+			<!-- InstanceEndEditable --> <span><a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> <a href="http://validator.w3.org/check/referer">W3C</a> <a href="http://www-1.unl.edu/feeds/">RSS</a> </span><a href="http://www.unl.edu/" title="UNL Home"><img src="/ucomm/templatedependents/templatecss/images/wordmark.png" alt="UNL's wordmark" id="wordmark" /></a></div>
+	</div>
+</div>
 
-<div id="navigation">
-<h3 id="sec_nav">Navigation</h3>
-<?php echo $this->navigation; ?>
-</div><!-- close navigation -->
-
-<div id="maincontentarea">
-<?php
-	UNL_UCBCN::displayRegion($this->output);
-?>
-</div><!-- close main content area -->
-
-</div><!-- close main left -->
-
-<div id="right_area">
-<?php UNL_UCBCN::displayRegion($this->right); ?>
-</div><!-- close right-area -->
-
-</div><!-- close container -->
+<!-- close footer -->
+<!-- sifr -->
+<script type="text/javascript" src="/ucomm/templatedependents/templatesharedcode/scripts/sifr_replacements.js"></script>
 </body>
-</html>
+<!-- InstanceEnd --></html>
