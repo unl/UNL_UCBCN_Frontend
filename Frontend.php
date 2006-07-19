02d63e568485a53fa7d5170dd867a7c9d5a66cd7
Alvin Woon <alvinwoon@gmail.com>
2006-07-19 16:13:13 +0000

    *** empty log message ***

--- Frontend.php
+++ Frontend.php
@@ -52,7 +52,7 @@ class UNL_UCBCN_Frontend extends UNL_UCBCN
 	function showNavigation()
 	{
 		$n = array();
-		$n[] = '<ul>';
+		$n[] = '<ul id="frontend_view_selector">';
 		$n[] = '<li><a href="'.$this->uri.'">Today\'s Events</a></li>';
 		$n[] = '<li><a href="'.$this->uri.'?'.date('\m=m\&\a\m\p\;\y=Y').'">This Month</a></li>';
 		$n[] = '<li><a href="'.$this->uri.'?'.date('\y=Y').'">This Year</a></li>';
