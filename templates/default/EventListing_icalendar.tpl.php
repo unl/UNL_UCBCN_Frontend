9ab2a94008128267cc91071734bfe701038a3718
Brett Bieber <brett.bieber@gmail.com>
2006-08-03 15:39:49 +0000

    Add icalendar output templates.

---
+++ templates/default/EventListing_icalendar.tpl.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This is the output for an event listing in icalendar format.
+ * @package UNL_UCBCN_Frontend
+ */
+?>
+BEGIN:VCALENDAR
+CALSCALE:GREGORIAN
+X-WR-TIMEZONE;VALUE=TEXT:US/Central
+METHOD:PUBLISH
+VERSION:2.0
+<?php
+foreach ($this->events as $e) {
+	$eventdatetime = $e->getLink('id','eventdatetime','event_id');
+	$out = array();
+	$out[] = 'BEGIN:VEVENT';
+	//$out[] = 'SEQUENCE:5';
+	if (isset($eventdatetime->starttime)) {
+        $out[] = 'DTSTART;TZID=US/Central:'.date('Ymd\THis',strtotime($eventdatetime->starttime));
+    }
+	//$out[] = 'DTSTAMP:20021028T011706Z';
+	$out[] = 'SUMMARY:'.$e->title;
+	$out[] = 'DESCRIPTION:'.$e->description;
+	//$out[] = 'URL:http://abc.com/pub/calendars/jsmith/mytime.ics';
+	//$out[] = 'UID:EC9439B1-FF65-11D6-9973-003065F99D04';
+	if (isset($eventdatetime->endtime)) {
+    	$out[] = 'DTEND;TZID=US/Central:'.date('Ymd\THis',strtotime($eventdatetime->endtime));
+    }
+	$out[] = 'END:VEVENT';
+	echo implode("\n",$out);
+} ?>
+END:VCALENDAR
---
+++ templates/default/Frontend_icalendar.tpl.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * This template file is for the icalendar and ics output formats.
+ */
+UNL_UCBCN::outputTemplate('UNL_UCBCN_EventListing','EventListing_icalendar');
+UNL_UCBCN::displayRegion($this->output);
+?>
