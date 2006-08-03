6b284ed8fd16cb72de9ed91cfe2ebc55ea025bf5
Brett Bieber <brett.bieber@gmail.com>
2006-08-03 22:11:40 +0000

    ical export formatting

--- templates/default/EventListing_icalendar.tpl.php
+++ templates/default/EventListing_icalendar.tpl.php
@@ -11,14 +11,14 @@ foreach ($this->events as $e) {
 	if (isset($eventdatetime->starttime)) {
         $out[] = 'DTSTART;TZID=US/Central:'.date('Ymd\THis',strtotime($eventdatetime->starttime));
     }
-	//$out[] = 'DTSTAMP:20021028T011706Z';
-	$out[] = 'SUMMARY:'.$e->title;
-	$out[] = 'DESCRIPTION:'.$e->description;
+	$out[] = 'DTSTAMP:'.date('Ymd\THis',strtotime($e->datecreated));
+	$out[] = 'SUMMARY:'.strip_tags($e->title);
+	$out[] = 'DESCRIPTION:'.strip_tags($e->description);
 	//$out[] = 'URL:http://abc.com/pub/calendars/jsmith/mytime.ics';
 	//$out[] = 'UID:EC9439B1-FF65-11D6-9973-003065F99D04';
 	if (isset($eventdatetime->endtime)) {
     	$out[] = 'DTEND;TZID=US/Central:'.date('Ymd\THis',strtotime($eventdatetime->endtime));
     }
 	$out[] = 'END:VEVENT';
-	echo implode("\n",$out);
+	echo implode("\n",$out)."\n";
 } ?>
