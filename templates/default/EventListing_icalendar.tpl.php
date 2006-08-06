de0bd07963e9359562888ae92d4a471148127252
Brett Bieber <brett.bieber@gmail.com>
2006-08-06 13:34:21 +0000

    Switch icalendar event listing frontend to use event instances instead of events.

--- templates/default/EventListing_icalendar.tpl.php
+++ templates/default/EventListing_icalendar.tpl.php
@@ -4,20 +4,19 @@
  * @package UNL_UCBCN_Frontend
  */
 foreach ($this->events as $e) {
-	$eventdatetime = $e->getLink('id','eventdatetime','event_id');
 	$out = array();
 	$out[] = 'BEGIN:VEVENT';
 	//$out[] = 'SEQUENCE:5';
-	if (isset($eventdatetime->starttime)) {
-        $out[] = 'DTSTART;TZID=US/Central:'.date('Ymd\THis',strtotime($eventdatetime->starttime));
+	if (isset($e->eventdatetime->starttime)) {
+        $out[] = 'DTSTART;TZID=US/Central:'.date('Ymd\THis',strtotime($e->eventdatetime->starttime));
     }
-	$out[] = 'DTSTAMP:'.date('Ymd\THis',strtotime($e->datecreated));
-	$out[] = 'SUMMARY:'.strip_tags($e->title);
-	$out[] = 'DESCRIPTION:'.strip_tags($e->description);
+	$out[] = 'DTSTAMP:'.date('Ymd\THis',strtotime($e->event->datecreated));
+	$out[] = 'SUMMARY:'.strip_tags($e->event->title);
+	$out[] = 'DESCRIPTION:'.strip_tags($e->event->description);
 	//$out[] = 'URL:http://abc.com/pub/calendars/jsmith/mytime.ics';
 	//$out[] = 'UID:EC9439B1-FF65-11D6-9973-003065F99D04';
 	if (isset($eventdatetime->endtime)) {
-    	$out[] = 'DTEND;TZID=US/Central:'.date('Ymd\THis',strtotime($eventdatetime->endtime));
+    	$out[] = 'DTEND;TZID=US/Central:'.date('Ymd\THis',strtotime($e->eventdatetime->endtime));
     }
 	$out[] = 'END:VEVENT';
 	echo implode("\n",$out)."\n";
