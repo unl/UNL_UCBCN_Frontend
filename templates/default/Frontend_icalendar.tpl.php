b292a0f8d2c869b6d7135f14cdd96d47577d0c8e
Brett Bieber <brett.bieber@gmail.com>
2006-08-06 13:46:01 +0000

    Line endings fix for ics output.

--- templates/default/Frontend_icalendar.tpl.php
+++ templates/default/Frontend_icalendar.tpl.php
@@ -3,5 +3,9 @@
  * This template file is for the icalendar and ics output formats.
  */
 UNL_UCBCN::outputTemplate('UNL_UCBCN_Frontend_Day','Frontend_Day_icalendar');
+ob_start();
 UNL_UCBCN::displayRegion($this->output);
+// Convert all line endings: line endings are windows-style, carriage-return, followed by a line feed
+$out = ob_get_contents();
+echo str_replace("\n","\r\n",$out);
 ?>
