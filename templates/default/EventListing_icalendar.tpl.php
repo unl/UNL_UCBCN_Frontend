16c7c48fc01a7c3f45d1f80c5b25ea77c87072a4
Brett Bieber <brett.bieber@gmail.com>
2006-08-12 04:06:25 +0000

    Fix error, displayRegion instead of display

--- templates/default/EventListing_icalendar.tpl.php
+++ templates/default/EventListing_icalendar.tpl.php
@@ -3,5 +3,5 @@
  * This is the output for an event listing in icalendar format.
  * @package UNL_UCBCN_Frontend
  */
-UNL_UCBCN::display($this->events);
+UNL_UCBCN::displayRegion($this->events);
 ?>
