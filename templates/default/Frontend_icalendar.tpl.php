572cb5235a52adbc3dd27a28ce8fd2507a11d0d6
Brett Bieber <brett.bieber@gmail.com>
2006-08-12 04:13:23 +0000

    Move the icalendar export up one level to Frontend_icalendar instead of Frontend_Day_icalendar.

--- templates/default/Frontend_icalendar.tpl.php
+++ templates/default/Frontend_icalendar.tpl.php
@@ -4,8 +4,40 @@
  */
 UNL_UCBCN::outputTemplate('UNL_UCBCN_Frontend_Day','Frontend_Day_icalendar');
 UNL_UCBCN::outputTemplate('UNL_UCBCN_EventInstance','EventInstance_icalendar');
-ob_start();
-UNL_UCBCN::displayRegion($this->output);
+ob_start(); ?>
+BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:UNL_UCBCN//UNL Event Publisher
+X-WR-CALNAME:<?php echo $this->calendar->name."\n"; ?>
+CALSCALE:GREGORIAN
+X-WR-TIMEZONE:US/Central
+METHOD:PUBLISH
+BEGIN:VTIMEZONE
+VERSION:2.0
+TZID:US/Central
+LAST-MODIFIED:<?php echo date('Ymd\THis\Z')."\n"; ?>
+BEGIN:STANDARD
+DTSTART:20051030T070000
+TZOFFSETTO:-0600
+TZOFFSETFROM:+0000
+TZNAME:CST
+END:STANDARD
+BEGIN:DAYLIGHT
+DTSTART:20060402T010000
+TZOFFSETTO:-0500
+TZOFFSETFROM:-0600
+TZNAME:CDT
+END:DAYLIGHT
+BEGIN:STANDARD
+DTSTART:20061029T020000
+TZOFFSETTO:-0600
+TZOFFSETFROM:-0500
+TZNAME:CST
+END:STANDARD
+END:VTIMEZONE
+<?php UNL_UCBCN::displayRegion($this->output); ?>
+END:VCALENDAR
+<?php
 // Convert all line endings: line endings are windows-style, carriage-return, followed by a line feed
 $out = ob_get_contents();
 ob_clean();
