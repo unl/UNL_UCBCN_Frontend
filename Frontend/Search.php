2d058352bc4a3c761ac8dbea69e3eef4a80e7a45
Brett Bieber <brett.bieber@gmail.com>
2006-11-01 20:44:07 +0000

    Implement  Request #26  	 Search should look in description, location, and event type

--- Frontend/Search.php
+++ Frontend/Search.php
@@ -30,26 +30,36 @@ class UNL_UCBCN_Frontend_Search extends UNL_UCBCN_Frontend
         $this->query = trim($this->query);
         if (!empty($this->query)) {
             $mdb2 = $this->calendar->getDatabaseConnection();
-            $sql = 'SELECT DISTINCT eventdatetime.id FROM event, eventdatetime, calendar_has_event WHERE ' .
-						'eventdatetime.event_id = event.id AND 
+            $sql = 'SELECT DISTINCT eventdatetime.id 
+					FROM event, event_has_eventtype, eventtype, eventdatetime, calendar_has_event, location
+					WHERE 
+						eventdatetime.event_id = event.id AND 
 						calendar_has_event.event_id = event.id AND 
 						calendar_has_event.status != \'pending\' AND 
-						calendar_has_event.calendar_id = '.$this->calendar->id.' AND ';
+						calendar_has_event.calendar_id = '.$this->calendar->id.' AND 
+						eventdatetime.location_id = location.id AND ';
 			if ($t = strtotime($this->query)) {
 				// This is a time...
 				$sql .= 'eventdatetime.starttime LIKE \''.date('Y-m-d',$t).'%\' ORDER BY eventdatetime.starttime';
 			} else {
 				// Do a textual search.
-				$sql .= 'event.title LIKE \'%'.$mdb2->escape($this->query).'%\' AND '.
+				$sql .= '(event.title LIKE \'%'.$mdb2->escape($this->query).'%\' OR '.
+				        'event.description LIKE \'%'.$mdb2->escape($this->query).'%\' OR '.
+				        '(location.name LIKE \'%'.$mdb2->escape($this->query).'%\') OR '.
+				        '(event_has_eventtype.event_id = event.id AND event_has_eventtype.eventtype_id = eventtype.id AND eventtype.name LIKE \'%'.$mdb2->escape($this->query).'%\')) AND '.
 				        '(eventdatetime.starttime>=\''.date('Y-m-d').' 00:00:00\' OR '.
 						'eventdatetime.endtime>\''.date('Y-m-d').' 00:00:00\') ORDER BY event.title';
 			}
 			
 			$res = $mdb2->query($sql);
-			$this->output = new UNL_UCBCN_EventListing();
-			$this->output->type = 'search';
-			while ($row = $res->fetchRow()) {
-				$this->output->events[] =  new UNL_UCBCN_EventInstance($row[0]);
+			if (!PEAR::isError($res)) {
+				$this->output = new UNL_UCBCN_EventListing();
+				$this->output->type = 'search';
+				while ($row = $res->fetchRow()) {
+					$this->output->events[] =  new UNL_UCBCN_EventInstance($row[0]);
+				}
+			} else {
+			    $this->output = new UNL_UCBCN_Error('Error, the search could not be completed: '.$res->getMessage());
 			}
         } else {
             $this->output = 'Enter a search string to search for events.';
