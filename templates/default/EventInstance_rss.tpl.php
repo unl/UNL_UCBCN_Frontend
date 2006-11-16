9e4ead408261d8f7665ada8e14646c2705221c26
Alvin Woon <alvinwoon@gmail.com>
2006-11-16 21:01:03 +0000

    format rss (take out h tags, put venue and time in one line)

--- templates/default/EventInstance_rss.tpl.php
+++ templates/default/EventInstance_rss.tpl.php
@@ -3,15 +3,15 @@
 	<link><?php echo $this->url; ?></link>
 	<description>
 		<?php
-		echo '&lt;h1&gt;'.UNL_UCBCN_Frontend::dbStringToHtml($this->event->description).'&lt;/h1&gt;';
+		echo '&lt;div&gt;'.UNL_UCBCN_Frontend::dbStringToHtml($this->event->description).'&lt;/div&gt;';
 		if (isset($this->event->subtitle)) echo '&lt;h2&gt;'.UNL_UCBCN_Frontend::dbStringToHtml($this->event->subtitle).'&lt;/h2&gt;';
-		echo date('l, F jS',strtotime($this->eventdatetime->starttime));
+		echo '&lt;small&gt;'.date('l, F jS',strtotime($this->eventdatetime->starttime)).'&lt;/small&gt;';
 		
 		if (isset($this->eventdatetime->starttime)) {
 			if (strpos($this->eventdatetime->starttime,'00:00:00')) {
-				echo '&lt;abbr class="dtstart" title="'.date(DATE_ISO8601,strtotime($this->eventdatetime->starttime)).'"&gt;All day&lt;/abbr&gt;';
+				echo ' | &lt;small&gt;&lt;abbr class="dtstart" title="'.date(DATE_ISO8601,strtotime($this->eventdatetime->starttime)).'"&gt;All day&lt;/abbr&gt;&lt;/small&gt;';
 			} else {
-	        	echo '&lt;abbr class="dtstart" title="'.date(DATE_ISO8601,strtotime($this->eventdatetime->starttime)).'"&gt;'.date('g:i a',strtotime($this->eventdatetime->starttime)).'&lt;/abbr&gt;';
+	        	echo ' | &lt;small&gt;&lt;abbr class="dtstart" title="'.date(DATE_ISO8601,strtotime($this->eventdatetime->starttime)).'"&gt;'.date('g:i a',strtotime($this->eventdatetime->starttime)).'&lt;/abbr&gt;&lt;/small&gt;';
 			}
 	    } else {
 	        echo 'Unknown';
@@ -19,15 +19,15 @@
 	    if (isset($this->eventdatetime->endtime) &&
 	    	($this->eventdatetime->endtime != $this->eventdatetime->starttime) &&
 	    	($this->eventdatetime->endtime > $this->eventdatetime->starttime)) {
-	    	echo '-&lt;abbr class="dtend" title="'.date(DATE_ISO8601,strtotime($this->eventdatetime->endtime)).'"&gt;'.date('g:i a',strtotime($this->eventdatetime->endtime)).'&lt;/abbr&gt;';
+	    	echo '-&lt;small&gt;&lt;abbr class="dtend" title="'.date(DATE_ISO8601,strtotime($this->eventdatetime->endtime)).'"&gt;'.date('g:i a',strtotime($this->eventdatetime->endtime)).'&lt;/abbr&gt;&lt;/small&gt;';
 	    }
 		$loc = $this->eventdatetime->getLink('location_id');
 		if (!PEAR::isError($loc)) {
-			echo '&lt;div class="location"&gt;'.UNL_UCBCN_Frontend::dbStringToHtml($loc->name);
+			echo ' | &lt;small&gt;'.UNL_UCBCN_Frontend::dbStringToHtml($loc->name);
 			if (isset($this->eventdatetime->room)) {
 			    echo ' Room:'.UNL_UCBCN_Frontend::dbStringToHtml($this->eventdatetime->room);
 			}
-			echo '&lt;/div&gt;';
+			echo '&lt;/small&gt;';
 		} ?>
 	</description>
 	<pubDate><?php echo date('r',strtotime($this->event->datecreated)); ?></pubDate>
