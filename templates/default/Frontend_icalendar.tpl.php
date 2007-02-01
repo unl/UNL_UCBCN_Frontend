c6018fa93b89e318bd4053a7c5109d31b7cfce89
Brett Bieber <brett.bieber@gmail.com>
2007-02-01 19:02:27 +0000

    Event listing output format for icalendar.

--- templates/default/Frontend_icalendar.tpl.php
+++ templates/default/Frontend_icalendar.tpl.php
@@ -3,6 +3,7 @@
  * This template file is for the icalendar and ics output formats.
  */
 UNL_UCBCN::outputTemplate('UNL_UCBCN_Frontend_Day','Frontend_Day_icalendar');
+UNL_UCBCN::outputTemplate('UNL_UCBCN_EventListing','EventListing_icalendar');
 UNL_UCBCN::outputTemplate('UNL_UCBCN_EventInstance','EventInstance_icalendar');
 ob_start(); ?>
 BEGIN:VCALENDAR
