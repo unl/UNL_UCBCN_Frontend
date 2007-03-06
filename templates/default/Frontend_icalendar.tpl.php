efaaf0dbe6f8f817c911a5ba2c4da2eb5f6127f7
Brett Bieber <brett.bieber@gmail.com>
2007-03-06 19:21:24 +0000

    Fix upcoming subscription ics feed.

--- templates/default/Frontend_icalendar.tpl.php
+++ templates/default/Frontend_icalendar.tpl.php
@@ -4,6 +4,7 @@
  */
 UNL_UCBCN::outputTemplate('UNL_UCBCN_Frontend_Day','Frontend_Day_icalendar');
 UNL_UCBCN::outputTemplate('UNL_UCBCN_Frontend_Search','Frontend_Day_icalendar');
+UNL_UCBCN::outputTemplate('UNL_UCBCN_Frontend_Upcoming','Frontend_Day_icalendar');
 UNL_UCBCN::outputTemplate('UNL_UCBCN_EventListing','EventListing_icalendar');
 UNL_UCBCN::outputTemplate('UNL_UCBCN_EventInstance','EventInstance_icalendar');
 ob_start(); ?>
