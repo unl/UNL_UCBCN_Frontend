ec4bdc2f45f7cad4ee80ebf2885fd35531740a69
Brett Bieber <brett.bieber@gmail.com>
2007-08-14 17:27:31 +0000

    Change default calendar subscription to upcoming event list.

--- templates/default/Frontend_Upcoming.tpl.php
+++ templates/default/Frontend_Upcoming.tpl.php
@@ -1,4 +1,7 @@
 <h1>Upcoming Events</h1>
 <?php
     UNL_UCBCN::displayRegion($this->output);
-?>
+    echo '<p id="feeds">
+            <a id="icsformat" title="ics format for upcoming events" href="'.UNL_UCBCN_Frontend::reformatURL($this->url,array('format'=>'ics')).'">ics format for upcoming events</a>
+            <a id="rssformat" title="rss format for upcoming events" href="'.UNL_UCBCN_Frontend::reformatURL($this->url,array('format'=>'rss')).'">rss format for upcoming events</a>
+            </p>'; ?>
