0e0e02fde75a4727ac6846451ebc590d74a98cd1
Brett Bieber <brett.bieber@gmail.com>
2007-08-29 16:14:46 +0000

    Formatting... add row for contact information.

--- templates/default/EventInstance.tpl.php
+++ templates/default/EventInstance.tpl.php
@@ -22,51 +22,43 @@ $endu = strtotime($this->eventdatetime->endtime);
 			</tr>
 		</thead>
 		<tbody>
-			<tr><td class="date">Date:</td>
-				<td><?php echo date('l, F jS',$startu); ?></td></tr>
-				
-				<tr class="alt"><td class="date">Time:</td>	
-					<td><?php
-					if (isset($this->eventdatetime->starttime)) {
-						if (strpos($this->eventdatetime->starttime,'00:00:00')) {
-							echo '<abbr class="dtstart" title="'.date(DATE_ISO8601, $startu).'">All day</abbr>';
-						} else {
-				        	echo '<abbr class="dtstart" title="'.date(DATE_ISO8601, $startu).'">'.date('g:i a', $startu).'</abbr>';
-						}
-				    } else {
-				        echo 'Unknown';
-				    }
-				    if (isset($this->eventdatetime->endtime) &&
-				    	($this->eventdatetime->endtime != $this->eventdatetime->starttime) &&
-				    	($this->eventdatetime->endtime > $this->eventdatetime->starttime)) {
-				    	if (substr($this->eventdatetime->endtime,0,10) != substr($this->eventdatetime->starttime,0,10)) {
-				    	    // Not on the same day
-				    	    if (strpos($this->eventdatetime->endtime,'00:00:00')) {
-				    	        echo '-<abbr class="dtend" title="'.date(DATE_ISO8601, $endu).'">'.date('l, F jS', $endu).'</abbr>';
-				    	    } else {
-				    	        echo '-<abbr class="dtend" title="'.date(DATE_ISO8601, $endu).'">'.date('l, F jS g:i a', $endu).'</abbr>';
-				    	    }
-				    	} else {
-   				    	    echo '-<abbr class="dtend" title="'.date(DATE_ISO8601, $endu).'">'.date('g:i a', $endu).'</abbr>';
-				    	}
-				    }
-					?></td>
+		<tr>
+			<td class="date">Date:</td>
+			<td><?php echo date('l, F jS',$startu); ?></td>
+		</tr>
+		<tr class="alt"><td class="date">Time:</td>	
+			<td><?php
+			if (isset($this->eventdatetime->starttime)) {
+				if (strpos($this->eventdatetime->starttime,'00:00:00')) {
+					echo '<abbr class="dtstart" title="'.date(DATE_ISO8601, $startu).'">All day</abbr>';
+				} else {
+		        	echo '<abbr class="dtstart" title="'.date(DATE_ISO8601, $startu).'">'.date('g:i a', $startu).'</abbr>';
+				}
+		    } else {
+		        echo 'Unknown';
+		    }
+		    if (isset($this->eventdatetime->endtime) &&
+		    	($this->eventdatetime->endtime != $this->eventdatetime->starttime) &&
+		    	($this->eventdatetime->endtime > $this->eventdatetime->starttime)) {
+		    	if (substr($this->eventdatetime->endtime,0,10) != substr($this->eventdatetime->starttime,0,10)) {
+		    	    // Not on the same day
+		    	    if (strpos($this->eventdatetime->endtime,'00:00:00')) {
+		    	        echo '-<abbr class="dtend" title="'.date(DATE_ISO8601, $endu).'">'.date('l, F jS', $endu).'</abbr>';
+		    	    } else {
+		    	        echo '-<abbr class="dtend" title="'.date(DATE_ISO8601, $endu).'">'.date('l, F jS g:i a', $endu).'</abbr>';
+		    	    }
+		    	} else {
+ 				    	    echo '-<abbr class="dtend" title="'.date(DATE_ISO8601, $endu).'">'.date('g:i a', $endu).'</abbr>';
+		    	}
+		    }
+			?></td>
 		</tr>
 		<tr>
 			<td class="date">Description:</td>	
 			<td><p class='description'>
 			
 			<?php echo UNL_UCBCN_Frontend::dbStringToHtml($this->event->description); ?></p>
-			<?php if (isset($this->event->listingcontactname) ||
-						isset($this->event->listingcontactphone) ||
-						isset($this->event->listingcontactemail)) { ?>
-		
-				Contact information:
-				<?php if (isset($this->event->listingcontactname)) echo '<div class="n">'.$this->event->listingcontactname.'</div>'; ?>
-				<?php if (isset($this->event->listingcontactphone)) echo '<div class="tel">'.$this->event->listingcontactphone.'</div>'; ?>
-				<?php if (isset($this->event->listingcontactemail)) echo '<div class="mailto">'.$this->event->listingcontactemail.'</div>'; ?>
-		
-			<?php } 
+			<?php 
 			if (isset($this->event->webpageurl)) {
 			    echo 'Website: <a class="url" href="'.UNL_UCBCN_Frontend::dbStringToHtml($this->event->webpageurl).'">'.UNL_UCBCN_Frontend::dbStringToHtml($this->event->webpageurl).'</a>';
 			}
@@ -86,21 +78,30 @@ $endu = strtotime($this->eventdatetime->endtime);
 				if ($loc = $this->eventdatetime->getLocation()) {
 					UNL_UCBCN::displayRegion($loc);
 				}
-				?></td>
+				?>
+			</td>
 		</tr>
 		<tr>
-			<td class="date">Subscription:</td>
+			<td class="date">Contact:</td>
 			<td>
-			<?php
-			echo '<p id="feeds">
-					<a id="icsformat" href="'.UNL_UCBCN_Frontend::reformatURL($this->url,array('format'=>'ics')).'">ics format for '.UNL_UCBCN_Frontend::dbStringToHtml($this->event->title).'</a>
-					<a id="rssformat" href="'.UNL_UCBCN_Frontend::reformatURL($this->url,array('format'=>'rss')).'">rss format for '.UNL_UCBCN_Frontend::dbStringToHtml($this->event->title).'</a>
-					</p>'; ?>
+			<?php 
+			    if (isset($this->event->listingcontactname) ||
+					isset($this->event->listingcontactphone) ||
+					isset($this->event->listingcontactemail)) {
+
+					if (isset($this->event->listingcontactname)) echo '<div class="n">'.$this->event->listingcontactname.'</div>';
+					if (isset($this->event->listingcontactphone)) echo '<div class="tel">'.$this->event->listingcontactphone.'</div>';
+					if (isset($this->event->listingcontactemail)) echo '<div class="mailto">'.$this->event->listingcontactemail.'</div>';
+				} ?>
 			</td>
 		</tr>
 		</tbody>
 		</table>
+		<?php
+			echo '<p id="feeds">
+			<a id="icsformat" href="'.UNL_UCBCN_Frontend::reformatURL($this->url,array('format'=>'ics')).'">ics format for '.UNL_UCBCN_Frontend::dbStringToHtml($this->event->title).'</a>
+			<a id="rssformat" href="'.UNL_UCBCN_Frontend::reformatURL($this->url,array('format'=>'rss')).'">rss format for '.UNL_UCBCN_Frontend::dbStringToHtml($this->event->title).'</a>
+			</p>'; ?>
 		</div>
-		
 	</div>
 </div>
