a645096a60052dbf7afe7520346ef8c397811180
Kyle Powers <kylepowers@gmail.com>
2010-11-10 21:08:37 +0000

    Adding passthrough template for json output.

--- www/templates/default/Frontend_json.tpl.php
+++ www/templates/default/Frontend_json.tpl.php
@@ -1,8 +1,5 @@
 [{}<?php
 UNL_UCBCN::outputTemplate('UNL_UCBCN_EventInstance','EventInstance_json');
-UNL_UCBCN::outputTemplate('UNL_UCBCN_Frontend_Day','Frontend_Day_xml');
-UNL_UCBCN::outputTemplate('UNL_UCBCN_Frontend_Search','Frontend_Day_xml');
-UNL_UCBCN::outputTemplate('UNL_UCBCN_Frontend_Upcoming','Frontend_Day_xml');
-UNL_UCBCN::outputTemplate('UNL_UCBCN_EventListing','EventListing_icalendar');
+UNL_UCBCN::outputTemplate('UNL_UCBCN_Frontend_Upcoming','Frontend_passthrough');
 UNL_UCBCN::displayRegion($this->output);
 ?>]
