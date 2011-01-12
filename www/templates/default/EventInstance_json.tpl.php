923f2722c327ece635cac33744f4a7f1d3e00adb
Michael Fairchild <mfairchild365@gmail.com>
2011-01-12 17:39:35 +0000

    removed uneeded json template file

--- www/templates/default/EventInstance_json.tpl.php
+++
@@ -1,9 +0,0 @@
-,
-{
-	"id":<?php echo $this->event->id; ?>,
-	"title":"<?php echo htmlspecialchars($this->event->title); ?>",
-	"start":<?php echo strtotime($this->eventdatetime->starttime); ?>,
-	"end":<?php echo strtotime($this->eventdatetime->endtime); ?>,
-	"url":"<?php echo htmlspecialchars_decode($this->url); ?>",
-	"allDay":false
-}
