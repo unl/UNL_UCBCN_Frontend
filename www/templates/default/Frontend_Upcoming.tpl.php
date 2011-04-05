8d8326711c36a07176e1fb701a6c9879f0f7c6a0
Michael Fairchild <mfairchild365@gmail.com>
2011-04-05 17:03:35 +0000

    We do not need the fullCalendar here.

--- www/templates/default/Frontend_Upcoming.tpl.php
+++ www/templates/default/Frontend_Upcoming.tpl.php
@@ -1,4 +1,3 @@
-<div class="calendar"></div>
 <h4 class="upcoming-header">Upcoming Events:</h4>
 <?php
     UNL_UCBCN::displayRegion($this->output);
@@ -6,18 +5,3 @@
             <a id="icsformat" title="ics format for upcoming events" href="'.UNL_UCBCN_Frontend::reformatURL($this->url,array('format'=>'ics')).'">ics format for upcoming events</a>
             <a id="rssformat" title="rss format for upcoming events" href="'.UNL_UCBCN_Frontend::reformatURL($this->url,array('format'=>'rss')).'">rss format for upcoming events</a>
             </p>'; ?>
-<script type='text/javascript'> 
-$(document).ready(function() {
-	$('.calendar').fullCalendar({
-		theme: true,
-		header: {
-			left: 'prev,next today',
-			center: 'title',
-			right: 'month,agendaWeek,agendaDay'
-		},
-		editable: false,
-		defaultView: 'agendaWeek',
-		events: '?upcoming=upcoming&limit=100&format=json'
-	});
-});
-</script> 
