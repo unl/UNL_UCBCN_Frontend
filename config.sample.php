c3d2923801bf69455ca1582ebb1447565f4c8a42
Brett Bieber <brett.bieber@gmail.com>
2013-04-30 17:04:40 -0500

    Add basic configuration file which references the vendor and backend

---
+++ config.sample.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file parses the configuration and connection details for the catalog database.
+ * 
+ * @package UNL_UCBCN
+ * @author bbieber
+ */
+
+function autoload($class)
+{
+    $class = str_replace(array('_', '\\'), '/', $class);
+    include $class . '.php';
+}
+
+spl_autoload_register('autoload');
+
+set_include_path(
+    __DIR__ . '/src' . PATH_SEPARATOR
+  . __DIR__ . '/backend/src' . PATH_SEPARATOR
+  . __DIR__ . '/vendor/php'
+);
+
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
+
+UNL\UCBCN\Frontend\Controller::$url = '/workspace/UNL_UCBCN/www/';
