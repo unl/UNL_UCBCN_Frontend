46bd91153b76dc807f47fe019a5a2576e8588419
Brett Bieber <brett.bieber@gmail.com>
2013-05-06 13:53:16 -0500

    Convert template to Savvy rendering model and clean up URL references

--- www/templates/default/html/Day.tpl.php
+++ www/templates/default/html/Day.tpl.php
@@ -2,26 +2,19 @@
 <div class="day_cal">
 <h4 class="sec_main">
 <?php
-$day = new Calendar_Day($context->year,$context->month,$context->day);
+$day = $context->getDateTime();
 echo date('l, F jS',$day->getTimeStamp());
-?> <a class="permalink" href="<?php echo $context->url; ?>">(link)</a>
+?> <a class="permalink" href="<?php echo $context->getURL(); ?>">(link)</a>
 </h4>
 <p id="day_nav">
 <?php
-    $prev = $day->prevDay('object');
-    echo '<a class="url prev" href="'.UNL_UCBCN_Frontend::formatURL(array(    'd'=>$prev->thisDay(),
-                                                            'm'=>$prev->thisMonth(),
-                                                            'y'=>$prev->thisYear(),
-                                                            'calendar'=>$context->calendar->id)).'">Previous Day</a> ';
-    $next = $day->nextDay('object');
-    echo '<a class="url next" href="'.UNL_UCBCN_Frontend::formatURL(array(    'd'=>$next->thisDay(),
-                                                            'm'=>$next->thisMonth(),
-                                                            'y'=>$next->thisYear(),
-                                                            'calendar'=>$context->calendar->id)).'">Next Day</a></p>';
+    echo '<a class="url prev" href="'.$frontend->getCalendarURL().date('Y/m/d', $day->getTimeStamp()-86400).'">Previous Day</a> ';
+    echo '<a class="url next" href="'.$frontend->getCalendarURL().date('Y/m/d', $day->getTimeStamp()+86400).'">Next Day</a></p>';
 
-    UNL_UCBCN::displayRegion($context->output);
+    echo $savvy->render($context, 'EventListing.tpl.php');
+
     echo '<p id="feeds">
-            <a id="icsformat" title="ics format for events on '.date('l, F jS',$day->getTimeStamp()).'" href="'.UNL_UCBCN_Frontend::reformatURL($context->url,array('format'=>'ics')).'">ics format for events on '.date('l, F jS',$day->getTimeStamp()).'</a>
-            <a id="rssformat" title="rss format for events on '.date('l, F jS',$day->getTimeStamp()).'" href="'.UNL_UCBCN_Frontend::reformatURL($context->url,array('format'=>'rss')).'">rss format for events on '.date('l, F jS',$day->getTimeStamp()).'</a>
+            <a id="icsformat" title="ics format for events on '.date('l, F jS',$day->getTimeStamp()).'" href="'.$context->getURL().'.ics">ics format for events on '.date('l, F jS',$day->getTimeStamp()).'</a>
+            <a id="rssformat" title="rss format for events on '.date('l, F jS',$day->getTimeStamp()).'" href="'.$context->getURL().'.rss">rss format for events on '.date('l, F jS',$day->getTimeStamp()).'</a>
             </p>'; ?>
 </div>
