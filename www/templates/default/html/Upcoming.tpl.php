ba79408c68d694d8dd02620c5b79c80b76a714db
Brett Bieber <brett.bieber@gmail.com>
2013-05-06 14:38:10 -0500

    Render events with EventListing template and use $frontend for URLs

--- www/templates/default/html/Upcoming.tpl.php
+++ www/templates/default/html/Upcoming.tpl.php
@@ -1,7 +1,7 @@
 <h4 class="upcoming-header">Upcoming Events:</h4>
 <?php
-    echo $savvy->render($context->output);
+    echo $savvy->render($context, 'EventListing.tpl.php');
     echo '<p id="feeds">
-            <a id="icsformat" title="ics format for upcoming events" href="'.UNL_UCBCN_Frontend::reformatURL($context->url,array('format'=>'ics')).'">ics format for upcoming events</a>
-            <a id="rssformat" title="rss format for upcoming events" href="'.UNL_UCBCN_Frontend::reformatURL($context->url,array('format'=>'rss')).'">rss format for upcoming events</a>
+            <a id="icsformat" title="ics format for upcoming events" href="'.$frontend->getCalendarURL().'upcoming.ics">ics format for upcoming events</a>
+            <a id="rssformat" title="rss format for upcoming events" href="'.$frontend->getCalendarURL().'upcoming.rss">rss format for upcoming events</a>
             </p>'; ?>
