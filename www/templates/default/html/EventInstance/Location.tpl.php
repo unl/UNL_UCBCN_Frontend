cf29f124826b66b4677fbdb2ce4c744efcf7c4bf
Michael Fairchild <mfairchild365@gmail.com>
2013-12-04 11:13:14 -0600

    Remove the $row variable

--- www/templates/default/html/EventInstance/Location.tpl.php
+++ www/templates/default/html/EventInstance/Location.tpl.php
@@ -1,13 +1,11 @@
 <?php
-$row = "";
 if (isset($context->eventdatetime->location_id) && $context->eventdatetime->location_id) {
     $l = $context->eventdatetime->getLocation();
-    $row .= ' <span class="location">';
+    echo ' <span class="location">';
     if (isset($l->mapurl)) {
-        $row .= '<a class="mapurl" href="'.$savvy->dbStringtoHtml($l->mapurl).'">'.$savvy->dbStringtoHtml($l->name).'</a>';
+        echo '<a class="mapurl" href="'.$savvy->dbStringtoHtml($l->mapurl).'">'.$savvy->dbStringtoHtml($l->name).'</a>';
     } else {
-        $row .= $savvy->dbStringtoHtml($l->name);
+        echo $savvy->dbStringtoHtml($l->name);
     }
-    $row .= '</span>';
+    echo '</span>';
 }
-echo $row;
