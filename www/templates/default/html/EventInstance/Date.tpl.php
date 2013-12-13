7622b563f7d851decae7852cfcefdd450f4f9110
Michael Fairchild <mfairchild365@gmail.com>
2013-12-13 15:50:16 -0600

    Add classes to let css decide what should be shown

--- www/templates/default/html/EventInstance/Date.tpl.php
+++ www/templates/default/html/EventInstance/Date.tpl.php
@@ -1,35 +1,74 @@
 <?php
-//Convert the times to something we can use.
-$startu = strtotime($context->eventdatetime->starttime);
-$endu = strtotime($context->eventdatetime->endtime);
-
-//get the start time
-if (isset($context->eventdatetime->starttime)) {
-    echo '<abbr class="dtstart" title="'.date('c', $startu).'">';
-    if ($context->isAllDay()) {
-        echo 'All day';
-    } else {
-        echo date('g:i a', $startu);
-    }
-    echo '</abbr>';
-} else {
-    echo 'Unknown';
+$classes = array('date-wrapper');
+if ($context->isAllDay()) {
+    $classes[] = 'all-day';
 }
 
-//get the end time
-if (isset($context->eventdatetime->endtime) &&
-    ($context->eventdatetime->endtime != $context->eventdatetime->starttime) &&
-    ($context->eventdatetime->endtime > $context->eventdatetime->starttime)) {
-
-    echo '-<abbr class="dtend" title="'.date(DATE_ISO8601, $endu).'">';
-    if ($context->isOngoing()) {
-        if (strpos($context->eventdatetime->endtime,'00:00:00')) {
-            echo date('M jS', $endu);
-        } else {
-            echo date('M jS g:i a', $endu);
-        }
-    } else {
-        echo date('g:i a', $endu);
-    }
-    echo '</abbr>';
+if ($context->isInProgress()) {
+    $classes[] = 'in-progress';
 }
+
+if ($context->isOnGoing()) {
+    $classes[] = 'ongoing';
+}
+?>
+
+<span class="<?php echo implode(' ', $classes); ?>">
+    <?php
+    //Convert the times to something we can use.
+    $startu = strtotime($context->eventdatetime->starttime);
+    $endu = strtotime($context->eventdatetime->endtime);
+    
+    //get the start time
+    if (isset($context->eventdatetime->starttime)) {
+        ?>
+        <abbr class="dtstart" title="<?php echo date('c', $startu); ?>">
+            <span class="month"><?php echo date('M', $startu); ?></span>
+            <span class="day"><?php echo date('j', $startu); ?></span>
+            <span class="year"><?php echo date('Y', $startu); ?></span>
+            <span class="time">
+            <?php
+            if ($context->isAllDay()) {
+                ?>
+                All Day
+                <?php
+            } else {
+                if (date('i', $startu) == '00') {
+                    echo date('g', $startu);
+                } else {
+                    echo date('g:i', $startu);
+                }
+            }
+            ?>
+                <span class="am-pm"><?php echo date('a', $startu); ?></span>
+            </span>
+        </abbr>
+        <?php
+    } else {
+        echo 'Unknown';
+    }
+    
+    //get the end time
+    if ((isset($context->eventdatetime->endtime) 
+            && $context->eventdatetime->endtime > $context->eventdatetime->starttime)
+            && (!$context->isAllDay() || $context->isOngoing())) {
+        ?>
+        -<abbr class="dtend" title="<?php date(DATE_ISO8601, $endu) ?>">
+            <span class="month"><?php echo date('M', $endu); ?></span>
+            <span class="day"><?php echo date('j', $endu); ?></span>
+            <span class="year"><?php echo date('Y', $endu); ?></span>
+            <span class="time">
+                <?php
+                if (date('i', $endu) == '00') {
+                    echo date('g', $endu);
+                } else {
+                    echo date('g:i', $endu);
+                }
+                ?>
+                <span class="am-pm"><?php echo date('a', $endu); ?></span>
+            </span>
+        </abbr>
+        <?php
+    }
+    ?>
+</span>
