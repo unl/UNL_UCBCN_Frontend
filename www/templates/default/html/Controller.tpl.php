4f274e384e6cea0375e24c58841baf6e826a973c
Michael Fairchild <mfairchild365@gmail.com>
2013-12-13 09:36:02 -0600

    Move sidebar content out of controller template

--- www/templates/default/html/Controller.tpl.php
+++ www/templates/default/html/Controller.tpl.php
@@ -131,38 +131,13 @@ if (!isset($GLOBALS['unl_template_dependents'])) {
                             <a href="#" title="close search tip">(close message)</a>
                         </p>
                     </form>
-                    <?php if (isset($context->right)) { ?>
-                        <div class="col left">
-                            <div id="monthwidget"><?php echo $savvy->render($context->right); ?></div>
-                            <div class="cal_widget">
-                                <h3>Contribute/Learn More</h3>
-                                <ul>
-                                    <li id="login_list"><a id="frontend_login" href="<?php echo $context->manageruri; ?>">Submit an Event</a> </li>
-                                    <li><a href="http://www1.unl.edu/wdn/wiki/UNL_Calendar_Documentation">Learn More</a></li>
-                                    <li><a href="http://www1.unl.edu/comments/">Provide Feedback</a> </li>
-                                </ul></div>
-
-
-                            <div id="subscribe" onmouseover="if(!g_bH){document.getElementById('droplist').style.display='block';}" onmouseout="if(!g_bH){document.getElementById('droplist').style.display='none';}">
-                                <span>Subscribe to this calendar</span>
-                                <ul id="droplist">
-                                    <li id="eventrss"><a href="<?php echo $frontend->formatURL(array('calendar'=>$context->getCalendar()->id,'upcoming'=>'upcoming','format'=>'rss')); ?>&amp;limit=100" title="RSS feed">RSS feed</a></li>
-                                    <li id="eventical"><a href="<?php echo $frontend->formatURL(array('calendar'=>$context->getCalendar()->id,'upcoming'=>'upcoming','format'=>'ics')); ?>&amp;limit=100" title=".ical format">.ical format</a></li>
-                                </ul>
-                            </div>
-
-                        </div>
-                        <div id="updatecontent" class="three_col right">
-                            <?php echo $savvy->render($context->output); ?>
-                        </div>
-
-                    <?php } else {
+                    <?php 
                         $template = null;
                         if ($context->output->getRawObject() instanceof Exception) {
                             $template = 'Exception.tpl.php';
                         }
                         echo $savvy->render($context->output, $template);
-                    } ?>
+                    ?>
                 </div>
             </div>
             <!-- InstanceEndEditable -->
