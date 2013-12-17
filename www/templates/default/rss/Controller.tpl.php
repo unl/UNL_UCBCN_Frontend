25388799689c555d3f8ac759254f7612997701c5
Michael Fairchild <mfairchild365@gmail.com>
2013-12-17 14:45:37 -0600

    Fix the RSS output

--- www/templates/default/rss/Controller.tpl.php
+++ www/templates/default/rss/Controller.tpl.php
@@ -1,17 +1,13 @@
 <rss version="2.0">
     <channel>
-        <title><?php echo $context->calendar->name; ?> Events</title>
-        <link><?php echo UNL_UCBCN_Frontend::formatURL(array('calendar'=>$context->calendar->id)); ?></link>
-        <description>Events for <?php echo $context->calendar->name; ?></description>
+        <title><?php echo $context->options['calendar']->name; ?> Events</title>
+        <link><?php echo $context->options['calendar']->getURL(); ?></link>
+        <description>Events for <?php echo $context->options['calendar']->name; ?></description>
         <language>en-us</language>
         <generator>UNL_UCBCN_Frontend-3.0</generator>
         <lastBuildDate><?php echo date('r'); ?></lastBuildDate>
         <?php
-        UNL_UCBCN::outputTemplate('UNL_UCBCN_Frontend_Day','Frontend_Day_rss');
-        UNL_UCBCN::outputTemplate('UNL_UCBCN_EventListing','EventListing_rss');
-        UNL_UCBCN::outputTemplate('UNL_UCBCN_EventInstance','EventInstance_rss');
-        UNL_UCBCN::outputTemplate('UNL_UCBCN_Frontend_Upcoming','Frontend_hcalendar');
-        UNL_UCBCN::outputTemplate('UNL_UCBCN_Frontend_Search','Frontend_Day_rss');
-        echo $savvy->render($context->output); ?>
+        echo $savvy->render($context->output);
+        ?>
     </channel>
 </rss>
--- www/templates/default/rss/EventListing.tpl.php
+++ www/templates/default/rss/EventListing.tpl.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * This is the output for an event listing in rss format.
- * @package UNL_UCBCN_Frontend
- */
-UNL_UCBCN::outputTemplate('UNL_UCBCN_EventInstance','EventInstance_rss');
-echo $savvy->render($context->events);
-?>
+foreach ($context as $eventInstance) {
+    echo $savvy->render($eventInstance);
+}
