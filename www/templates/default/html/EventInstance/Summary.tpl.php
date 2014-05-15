0399d3bd00483323a53312b444ef9eab952142aa
Brett Bieber <brett.bieber@gmail.com>
2014-05-15 11:27:48 -0500

    Use heading for event summary line

--- www/templates/default/html/EventInstance/Summary.tpl.php
+++ www/templates/default/html/EventInstance/Summary.tpl.php
@@ -1,3 +1,3 @@
-<a class="url summary" href="<?php echo $frontend->getEventURL($context->getRawObject()) ?>">
+<h2><a class="url summary" href="<?php echo $frontend->getEventURL($context->getRawObject()) ?>">
     <?php echo $savvy->dbStringtoHtml($context->event->title) ?>
-</a>
+</a></h2>
