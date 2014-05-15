1340a39fe7f904320543c53157d68554d771f35a
Brett Bieber <brett.bieber@gmail.com>
2014-05-15 11:14:47 -0500

    Whitespace/alignment

--- www/templates/default/html/EventListing.tpl.php
+++ www/templates/default/html/EventListing.tpl.php
@@ -1,30 +1,30 @@
-    <div class="vcalendar">
-        <?php
-        foreach ($context as $eventinstance) {
-            //Start building an array of row classes
-            $row_classes = array('vevent');
+<div class="vcalendar">
+    <?php
+    foreach ($context as $eventinstance) {
+        //Start building an array of row classes
+        $row_classes = array('vevent');
 
-            if ($eventinstance->isAllDay()) {
-                $row_classes[] = 'all-day';
-            }
-
-            if ($eventinstance->isInProgress()) {
-                $row_classes[] = 'in-progress';
-            }
-
-            if ($eventinstance->isOnGoing()) {
-                $row_classes[] = 'ongoing';
-            }
-
-            ?>
-            <div class="<?php echo implode(' ', $row_classes) ?>">
-                    <?php echo $savvy->render($eventinstance, 'EventInstance/Summary.tpl.php') ?>
-                    <?php echo $savvy->render($eventinstance, 'EventInstance/Date.tpl.php') ?>
-                    <?php echo $savvy->render($eventinstance, 'EventInstance/Location.tpl.php') ?>
-                    <?php echo $savvy->render($eventinstance, 'EventInstance/Description.tpl.php') ?>
-            </div>
-            <?php
+        if ($eventinstance->isAllDay()) {
+            $row_classes[] = 'all-day';
         }
+
+        if ($eventinstance->isInProgress()) {
+            $row_classes[] = 'in-progress';
+        }
+
+        if ($eventinstance->isOnGoing()) {
+            $row_classes[] = 'ongoing';
+        }
+
         ?>
-    </div>
+        <div class="<?php echo implode(' ', $row_classes) ?>">
+                <?php echo $savvy->render($eventinstance, 'EventInstance/Summary.tpl.php') ?>
+                <?php echo $savvy->render($eventinstance, 'EventInstance/Date.tpl.php') ?>
+                <?php echo $savvy->render($eventinstance, 'EventInstance/Location.tpl.php') ?>
+                <?php echo $savvy->render($eventinstance, 'EventInstance/Description.tpl.php') ?>
+        </div>
+        <?php
+    }
+    ?>
+</div>
 
