345d05c1189c15a6ccf410006211ee21d50aee94
Brett Bieber <brett.bieber@gmail.com>
2014-05-15 13:29:16 -0500

    Collapse whitespace for spacing

--- www/templates/default/html/EventInstance/Date.tpl.php
+++ www/templates/default/html/EventInstance/Date.tpl.php
@@ -25,16 +25,9 @@ $endtime = $context->getEndTime();
     //get the start time
     if (!empty($starttime)) {
         ?>
-        <abbr class="dtstart" title="<?php echo date('c', $startu); ?>">
-            <span class="month"><?php echo date('M', $startu); ?></span>
-            <span class="day"><?php echo date('j', $startu); ?></span>
-            <span class="year"><?php echo date('Y', $startu); ?></span>
-            <span class="time">
-            <?php
+        <abbr class="dtstart" title="<?php echo date('c', $startu); ?>"><span class="month"><?php echo date('M', $startu); ?></span><span class="day"><?php echo date('j', $startu); ?></span><span class="year"><?php echo date('Y', $startu); ?></span><span class="time"><?php
             if ($context->isAllDay()) {
-                ?>
-                All Day
-                <?php
+                ?>All Day<?php
             } else {
                 if (date('i', $startu) == '00') {
                     echo date('g', $startu);
@@ -42,11 +35,7 @@ $endtime = $context->getEndTime();
                     echo date('g:i', $startu);
                 }
             }
-            ?>
-                <span class="am-pm"><?php echo date('a', $startu); ?></span>
-            </span>
-        </abbr>
-        <?php
+            ?><span class="am-pm"><?php echo date('a', $startu); ?></span></span></abbr><?php
     } else {
         echo 'Unknown';
     }
@@ -55,23 +44,13 @@ $endtime = $context->getEndTime();
     if ((!empty($endtime) 
             && $endu > $startu)
             && (!$context->isAllDay() || $context->isOngoing())) {
-        ?>
-        -<abbr class="dtend" title="<?php date(DATE_ISO8601, $endu) ?>">
-            <span class="month"><?php echo date('M', $endu); ?></span>
-            <span class="day"><?php echo date('j', $endu); ?></span>
-            <span class="year"><?php echo date('Y', $endu); ?></span>
-            <span class="time">
-                <?php
+        ?>-<abbr class="dtend" title="<?php date(DATE_ISO8601, $endu) ?>"><span class="month"><?php echo date('M', $endu); ?></span><span class="day"><?php echo date('j', $endu); ?></span><span class="year"><?php echo date('Y', $endu); ?></span><span class="time"><?php
                 if (date('i', $endu) == '00') {
                     echo date('g', $endu);
                 } else {
                     echo date('g:i', $endu);
                 }
-                ?>
-                <span class="am-pm"><?php echo date('a', $endu); ?></span>
-            </span>
-        </abbr>
-        <?php
+                ?><span class="am-pm"><?php echo date('a', $endu); ?></span></span></abbr><?php
     }
     ?>
 </span>
