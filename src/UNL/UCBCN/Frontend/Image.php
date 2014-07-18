be90093e081b4ac4c832cc1ef55da6cb4be6bb26
Michael Fairchild <mfairchild365@gmail.com>
2014-07-18 14:40:45 -0500

    Implement the Image model
    It looks like what was there was a 'best guess'.  Because it was extending an event, and $this->reload() was being called in the constructor, it would end up in an infinite loop.  It is better to not extend record classes for this reason.

--- src/UNL/UCBCN/Frontend/Image.php
+++ src/UNL/UCBCN/Frontend/Image.php
@@ -3,24 +3,30 @@ namespace UNL\UCBCN\Frontend;
 
 use UNL\UCBCN\Event;
 
-class Image extends Event
+class Image
 {
+    public $event = false;
+
     /**
      * Use for displaying the image for a given event.
      * $options['id'] must be set to the event.id which has the image.
      *
-     * @return void
+     * @param array $options
+     * @throws UnexpectedValueException
+     * @return \UNL\UCBCN\Frontend\Image
      */
     public function __construct($options = array())
-    {
+    {      
         if (!isset($options['id'])) {
-            throw new Exception('You must pass an event id', 500);
+            throw new UnexpectedValueException('You must pass an event id', 500);
         }
-        $this->id = (int) $options['id'];
-        $this->reload();
 
-        if (!isset($this->imagedata)) {
-            throw new Exception('No image data', 404);
+        if (!$this->event = Event::getById($options['id'])) {
+            throw new UnexpectedValueException('The event could not be found', 404);
+        }
+
+        if (!isset($this->event->imagedata)) {
+            throw new UnexpectedValueException('No image data', 404);
         }
     }
 }
