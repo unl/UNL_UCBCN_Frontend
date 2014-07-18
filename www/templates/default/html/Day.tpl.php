43cdfa3c2cbc22950405217fc74d7df5df49cb48
Kevin Abel <kevin.abel.0@gmail.com>
2014-07-18 15:03:07 -0500

    Change grid breakpoints for the Day template

--- www/templates/default/html/Day.tpl.php
+++ www/templates/default/html/Day.tpl.php
@@ -1,8 +1,8 @@
 <div class="wdn-grid-set">
-    <section class="bp1-wdn-col-one-third">
+    <section class="bp2-wdn-col-one-third">
         <?php echo $savvy->render($context, 'sidebar.tpl.php'); ?>
     </section>
-    <section id="updatecontent" class="day_cal bp1-wdn-col-two-thirds">
+    <section id="updatecontent" class="day_cal bp2-wdn-col-two-thirds">
         <?php
         echo $savvy->render($context, 'hcalendar/Day.tpl.php');
         ?>
