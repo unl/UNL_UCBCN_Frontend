699326d21c29186d107c8b330a3cd235029858d2
Kevin Abel <kevin.abel.0@gmail.com>
2014-07-18 15:02:21 -0500

    Adjust markup of subscribe links

--- www/templates/default/html/sidebar.tpl.php
+++ www/templates/default/html/sidebar.tpl.php
@@ -5,7 +5,7 @@
 <div id="subscribe">
     <span>Subscribe to this calendar</span>
     <ul id="droplist">
-        <li id="eventrss"><a href="<?php echo $frontend->getUpcomingURL(); ?>?format=rss&amp;limit=100" title="RSS feed">RSS feed</a></li>
-        <li id="eventical"><a href="<?php echo $frontend->getUpcomingURL(); ?>?format=ics&amp;limit=100" title=".ical format">.ical format</a></li>
+        <li id="eventrss"><a href="<?php echo $frontend->getUpcomingURL(); ?>?format=rss&amp;limit=100" title="RSS feed of upcoming events" class="wdn-icon-rss-squared">RSS</a></li>
+        <li id="eventical"><a href="<?php echo $frontend->getUpcomingURL(); ?>?format=ics&amp;limit=100" title="ICS format of upcoming events" class="wdn-icon-calendar">ICS</a></li>
     </ul>
 </div>
