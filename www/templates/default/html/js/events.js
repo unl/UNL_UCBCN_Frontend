3209335edc6e329850982d88282efdb01225e67a
Kevin Abel <kevin.abel.0@gmail.com>
2014-07-21 11:57:51 -0500

    Add script to add "today" class to monthwidget

--- www/templates/default/html/js/events.js
+++ www/templates/default/html/js/events.js
@@ -1,7 +1,22 @@
 require(['jquery', 'wdn'], function($, WDN) {
-	var $progress = $('<progress>');
+	var $progress = $('<progress>'),
+		months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 	
 	$(function() {
-		WDN.log('Something should happen')
+		var $monthWidget = $('.wp-calendar')
+		if ($monthWidget.length) {
+			var now = new Date();
+				month = $.trim($('.monthvalue', $monthWidget).text()),
+				year = $.trim($('.yearvalue', $monthWidget).text());
+			
+			if (year == now.getFullYear() && month == months[now.getMonth()]) {
+				$('td', $monthWidget).not('.prev,.next').each(function() {
+					if ($.trim($(this).text()) == now.getDate()) {
+						$(this).addClass('today');
+						return false;
+					}
+				});
+			}
+		}
 	});
 });
