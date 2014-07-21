11de9d5e7983823db3719f21562778f5089502ff
Kevin Abel <kevin.abel.0@gmail.com>
2014-07-21 17:07:31 -0500

    Remove the day suffix from day heading
    Closes #78

--- www/templates/default/hcalendar/Day.tpl.php
+++ www/templates/default/hcalendar/Day.tpl.php
@@ -1,5 +1,5 @@
-<?php $formattedDate = $context->getDateTime()->format('l, F jS'); ?>
-<h1 class="day-heading">
+<?php $formattedDate = $context->getDateTime()->format('l, F j'); ?>
+<h1 class="day-heading" data-datetime="<?php echo $context->getDateTime()->format('c') ?>">
     <?php echo $formattedDate ?>
     <a class="permalink" href="<?php echo $context->getURL(); ?>" title="permalink"><span class="wdn-icon-link"></span></a>
     <a title="ics format for events on <?php echo $formattedDate ?>" href="<?php echo $context->getURL() ?>.ics"><span class="wdn-icon-calendar"></span></a>
