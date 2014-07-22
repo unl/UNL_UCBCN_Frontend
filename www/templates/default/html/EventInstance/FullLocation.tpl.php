31ec7c4bcc0e63f29799cb260b9b8a0daa675a62
Kevin Abel <kevin.abel.0@gmail.com>
2014-07-22 17:13:45 -0500

    Hide location if not location information provided
    Closes #85

--- www/templates/default/html/EventInstance/FullLocation.tpl.php
+++ www/templates/default/html/EventInstance/FullLocation.tpl.php
@@ -1,11 +1,16 @@
-<?php $location = $context->eventdatetime->getLocation(); ?>
+<?php 
+$location = $context->eventdatetime->getLocation();
+$room = isset($location->room) ? $location->room : $context->eventdatetime->room;
+$directions = isset($location->directions) ? $location->directions : $context->eventdatetime->directions
+?>
+<?php if (isset($location->name) || $room || $directions || isset($location->streetaddress1)): ?>
 <div class="location eventicon-location">
 <?php if (isset($location->mapurl)): ?>
-    <a class="mapurl" href="<?php echo $location->mapurl ?>"><?php echo $savvy->dbStringtoHtml($location->name) ?></a>
+    <a class="mapurl" href="<?php echo $location->mapurl ?>"><?php echo ucwords(strtolower($location->name)) ?></a>
 <?php else: ?>
-    <?php echo $savvy->dbStringtoHtml($location->name) ?>
+    <?php echo ucwords(strtolower($location->name)) ?>
 <?php endif; ?>
-<?php if ($room = isset($location->room) ? $location->room : $context->eventdatetime->room): ?>
+<?php if ($room): ?>
     <span class="room">Room: <?php echo $room ?></span>
 <?php endif; ?>
 <?php if (isset($location->streetaddress1)): ?>
@@ -22,10 +27,11 @@
         <?php endif; ?>
     </div>
 <?php endif; ?>
-<?php if ($directions = isset($location->directions) ? $location->directions : $context->eventdatetime->directions): ?>
+<?php if ($directions): ?>
     <div class="directions">Directions: <?php echo $savvy->dbStringtoHtml($directions) ?></div>
 <?php endif ?>
 <?php if (isset($location->additionalpublicinfo)): ?>
     <div class="additionalinfo">Additional Info: <?php echo $savvy->dbStringtoHtml($location->additionalpublicinfo) ?></div>
 <?php endif ?>
 </div>
+<?php endif; ?>
