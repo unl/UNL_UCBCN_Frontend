a660bad6cf1992bb58764ed8040d88d21a761f02
Michael Fairchild <mfairchild365@gmail.com>
2014-07-22 14:10:03 -0500

    Clean up the code a little

--- www/templates/default/html/Search.tpl.php
+++ www/templates/default/html/Search.tpl.php
@@ -1,20 +1,20 @@
 <div class="day_cal">
-<?php
-if (is_a($context->output,'UNL_UCBCN_EventListing')) {
-    if ($dt = strtotime($context->query)) {
-        echo '<h1 class="results">Search results for events dated <span>'.date('F jS',$dt).'</span><a class="permalink" href="'.$context->url.'">(link)</a></h1>';
-    } else {
-        echo '<h1 class="results">Search results for "<span>'.htmlentities($context->query).'</span>"<a class="permalink" href="'.$context->url.'">(link)</a></h1>';
-    }
-    echo '<h3>'.count($context->output->events).' results</h3>';
-}
-echo $savvy->render($context->output);
+    <h1 class="results">
+        <?php
+        if ($dt = strtotime($context->search_query) && ($context->search_query != 'art')) {
+            echo 'Search results for events dated <span>'.date('F jS',$dt).'</span><a class="permalink" href="'.$context->getURL().'">(link)</a>';
+        } else {
+            echo 'Search results for "<span>'.htmlentities($context->search_query).'</span>"';
+        }
+        ?>
+    </h1>
+    <?php
 
-echo '<p id="feeds">
-            <a id="icsformat" title="ics format for search results" href="'.UNL_UCBCN_Frontend::reformatURL($context->url,array('format'=>'ics')).'">ics format for search results</a>
-            <a id="rssformat" title="rss format for search results" href="'.UNL_UCBCN_Frontend::reformatURL($context->url,array('format'=>'rss')).'">rss format for search results</a>
-            </p>';
-
-?>
-
+    echo '<span class="wdn-subhead">'.$context->count().' results.  <a class="permalink" href="'.$context->getURL().'">(link)</a></span>';
+    echo $savvy->render($context, 'EventListing.tpl.php');
+    ?>
+    <p id="feeds">
+        <a id="icsformat" title="ics format for search results" href="<?php echo $context->getURL()?>?format=ics">ics format for search results</a>
+        <a id="rssformat" title="rss format for search results" href="<?php echo $context->getURL()?>?format=rss">rss format for search results</a>
+    </p>
 </div>
