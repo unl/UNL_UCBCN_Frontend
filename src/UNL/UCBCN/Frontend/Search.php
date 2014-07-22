58925dced628c1627a679eeea512c34b0c8389c3
Michael Fairchild <mfairchild365@gmail.com>
2014-07-22 14:03:18 -0500

    Implement search

--- src/UNL/UCBCN/Frontend/Search.php
+++ src/UNL/UCBCN/Frontend/Search.php
@@ -27,41 +27,9 @@ namespace UNL\UCBCN\Frontend;
  * @license   http://www1.unl.edu/wdn/wiki/Software_License BSD License 
  * @link      http://code.google.com/p/unl-event-publisher/
  */
-class Search implements RoutableInterface
+class Search extends EventListing implements RoutableInterface
 {
-    /**
-     * Calendar we are searching
-     *
-     * @var object \UNL\UCBCN\Calendar
-     */
-    public $calendar;
-    
-    /**
-     * Listing of matching events or error.
-     * 
-     * @var UNL_UCBCN_EventListing
-     */
-    public $output;
-    
-    /**
-     * actual search string user entered
-     *
-     * @var string
-     */
-    public $query;
-    
-    /**
-     * Start time to search
-     */
-    public $starttime;
-    
-    /**
-     * end time to search
-     */
-    public $endtime;
-    
-    /** URL to this search results page. */
-    public $url;
+    public $search_query = '';
     
     /**
      * Constructs this search output.
@@ -70,66 +38,53 @@ class Search implements RoutableInterface
      */
     public function __construct($options=array())
     {
-        $this->view = 'search';
-        $this->setOptions($options);
-        $this->url = $this->getURL();
-        $this->run();
-    }
-    
-    /**
-     * Runs the query on the database from the calendar, currently supports two types of
-     * searches - textual or date and time hinted by passing a string supported by the strtotime() function.
-     * 
-     * Text searches:
-     * Title, Description, Location
-     * 
-     * @return void
-     */
-    public function run()
-    {
-        $this->query = trim($this->query);
-        if (!empty($this->query)) {
-            $mdb2 = $this->calendar->getDatabaseConnection();
-            $sql  = 'SELECT DISTINCT eventdatetime.id 
-                    FROM event, eventdatetime, calendar_has_event, location, 
-                      eventtype, event_has_eventtype
-                    WHERE
-                        event_has_eventtype.event_id = event.id AND
-                        event_has_eventtype.eventtype_id = eventtype.id AND
-                        eventdatetime.event_id = event.id AND 
-                        calendar_has_event.event_id = event.id AND 
-                        calendar_has_event.status != \'pending\' AND 
-                        calendar_has_event.calendar_id = '.$this->calendar->id.' AND 
-                        eventdatetime.location_id = location.id AND ';
-            if (($t = strtotime($this->query)) && ($this->query != 'art')) {
-                // This is a time...
-                $sql .= 'eventdatetime.starttime LIKE \''.date('Y-m-d', $t).'%\' ORDER BY eventdatetime.starttime';
-            } else {
-                // Do a textual search.
-                $sql .= 
-                        '(event.title LIKE \'%'.$mdb2->escape($this->query).'%\' OR '.
-                        '(eventtype.name LIKE \'%'.$mdb2->escape($this->query).'%\') OR '.
-
-                        'event.description LIKE \'%'.$mdb2->escape($this->query).'%\' OR '.
-                        '(location.name LIKE \'%'.$mdb2->escape($this->query).'%\')) AND '.
-                        '(eventdatetime.starttime>=\''.date('Y-m-d').' 00:00:00\' OR '.
-                        'eventdatetime.endtime>\''.date('Y-m-d').' 00:00:00\')
-
-                        ORDER BY eventdatetime.starttime ASC';
-            }
-            $res = $mdb2->query($sql);
-            if (!PEAR::isError($res)) {
-                $this->output       = new UNL_UCBCN_EventListing();
-                $this->output->type = 'search';
-                while ($row = $res->fetchRow()) {
-                    $this->output->events[] =  new UNL_UCBCN_EventInstance($row[0]);
-                }
-            } else {
-                $this->output = new UNL_UCBCN_Error('Error, the search could not be completed: '.$res->getMessage().'<br />Query:'.htmlentities($sql));
-            }
-        } else {
-            $this->output = 'Enter a search string to search for events.';
+        if (isset($options['q'])) {
+            $this->search_query = $options['q'];
         }
+        
+        parent::__construct($options);
+    }
+
+    /**
+     * Get the SQL for finding events
+     *
+     * @see \UNL\UCBCN\ActiveRecord\RecordList::getSQL()
+     */
+    function getSQL()
+    {
+        $sql = '
+                SELECT DISTINCT e.id as id, recurringdate.id as recurringdate_id
+                FROM eventdatetime as e
+                INNER JOIN event ON e.event_id = event.id
+                INNER JOIN calendar_has_event ON calendar_has_event.event_id = event.id
+                LEFT JOIN recurringdate ON (recurringdate.event_id = event.id AND recurringdate.unlinked = 0)
+                LEFT JOIN event_has_eventtype ON (event_has_eventtype.event_id = event.id)
+                LEFT JOIN eventtype ON (eventtype.id = event_has_eventtype.eventtype_id)
+                LEFT JOIN location ON (location.id = e.location_id)
+                WHERE
+                    calendar_has_event.calendar_id = ' . (int)$this->calendar->id . '
+                    AND calendar_has_event.status IN ("posted", "archived")
+                    AND  (';
+
+        if (($t = strtotime($this->search_query)) && ($this->search_query != 'art')) {
+            // This is a time...
+            $sql .= 'e.starttime LIKE \''.date('Y-m-d', $t).'%\' ORDER BY e.starttime';
+        } else {
+            // Do a textual search.
+            $sql .=
+                '(event.title LIKE \'%'.self::escapeString($this->search_query).'%\' OR '.
+                '(eventtype.name LIKE \'%'.self::escapeString($this->search_query).'%\') OR '.
+
+                'event.description LIKE \'%'.self::escapeString($this->search_query).'%\' OR '.
+                '(location.name LIKE \'%'.self::escapeString($this->search_query).'%\')) AND '.
+                '(e.starttime>=\''.date('Y-m-d').' 00:00:00\' OR '.
+                'e.endtime>\''.date('Y-m-d').' 00:00:00\')';
+        }
+
+        $sql.= ')
+                ORDER BY e.starttime ASC, event.title ASC';
+
+        return $sql;
     }
     
     /**
@@ -139,9 +94,13 @@ class Search implements RoutableInterface
      */
     public function getURL()
     {
-        return UNL_UCBCN_Frontend::formatURL(array( 'search'  => 'search',
-                                                    'q'       => urlencode($this->query),
-                                                    'calendar'=>$this->calendar->id));
+        $url = $this->options['calendar']->getURL() . 'search/';
+        
+        if (!empty($this->search_query)) {
+            $url .= $this->search_query . '/';
+        }
+        
+        return $url;
     }
     
 }
