746538845cbe0c4eedb2e98e9142ed4059eb0400
Kevin Abel <kevin.abel.0@gmail.com>
2014-07-22 15:46:04 -0500

    Update styles and markup for year view

--- www/templates/default/html/Year.tpl.php
+++ www/templates/default/html/Year.tpl.php
@@ -1,15 +1,8 @@
 <h1 class="year_main"><?php echo $context->year; ?></h1>
-<div class="year_cal">
-<?php echo $savvy->render($context->monthwidgets[0]); ?>
-<?php echo $savvy->render($context->monthwidgets[1]); ?>
-<?php echo $savvy->render($context->monthwidgets[2]); ?>
-<?php echo $savvy->render($context->monthwidgets[3]); ?>
-<?php echo $savvy->render($context->monthwidgets[4]); ?>
-<?php echo $savvy->render($context->monthwidgets[5]); ?>
-<?php echo $savvy->render($context->monthwidgets[6]); ?>
-<?php echo $savvy->render($context->monthwidgets[7]); ?>
-<?php echo $savvy->render($context->monthwidgets[8]); ?>
-<?php echo $savvy->render($context->monthwidgets[9]); ?>
-<?php echo $savvy->render($context->monthwidgets[10]); ?>
-<?php echo $savvy->render($context->monthwidgets[11]); ?>
+<div class="year_cal wdn-grid-set bp1-wdn-grid-set-halves bp2-wdn-grid-set-thirds">
+<?php foreach ($context->getRaw('monthwidgets') as $widget): ?>
+<div class="wdn-col">
+    <?php echo $savvy->render($widget) ?>
+</div>
+<?php endforeach; ?>
 </div>
