46664dd47be31408d2839cf60701658f83fe9b95
Kevin Abel <kevin.abel.0@gmail.com>
2014-07-23 09:22:10 -0500

    Add icons to week view and improve table caption with month

--- www/templates/default/html/Week.tpl.php
+++ www/templates/default/html/Week.tpl.php
@@ -5,15 +5,14 @@ $next = $context->getDateTime()->modify('+1 week');
 <div class="week_cal" id="month_viewcal">
     <table class="wp-calendar">
         <caption>
-            <span><a href="<?php echo $context->getPreviousURL(); ?>" id="prev_week" title="View events for week <?php echo $prev->format('W'); ?>">&lt;&lt; </a></span>
+            <span><a href="<?php echo $context->getPreviousURL(); ?>" id="prev_week" title="View events for week <?php echo $prev->format('W'); ?>" class="eventicon-angle-circled-left"></a></span>
             <span class="monthvalue">
-                <a href="<?php echo $context->getURL(); ?>">Week <?php echo $context->getDateTime()->format('W'); ?></a>
+                <a href="<?php echo $context->getURL(); ?>">Week <?php echo $context->getDateTime()->format('W'); ?>: <?php echo $context->getDateTime()->format('F'); ?></a>
             </span>
-            of
             <span class="yearvalue">
                 <a href="<?php echo $context->getYearURL(); ?>"><?php echo $context->getDateTime()->format('Y'); ?></a>
             </span>
-            <span><a href="<?php echo $context->getNextURL(); ?>" id="next_week" title="View events for week <?php echo $next->format('W'); ?>"> &gt;&gt;</a></span>
+            <span><a href="<?php echo $context->getNextURL(); ?>" id="next_week" title="View events for week <?php echo $next->format('W'); ?>" class="eventicon-angle-circled-right"></a></span>
         </caption>
         <thead>
         <?php
