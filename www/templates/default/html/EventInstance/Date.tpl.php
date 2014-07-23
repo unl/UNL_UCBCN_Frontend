e8dcd58a88ef0fe0c8b684d1439cf445b4893d90
Ryan Dee <ryan@skoolbus39.com>
2014-07-23 17:11:15 -0500

    En dashes for date/time ranges

--- www/templates/default/html/EventInstance/Date.tpl.php
+++ www/templates/default/html/EventInstance/Date.tpl.php
@@ -14,15 +14,14 @@ $endu = new DateTime($endtime);
     <?php if (!empty($starttime)): ?>
         <time class="dtstart" datetime="<?php echo $startu->format('c') ?>"><?php echo $startu->format('M. j, Y') ?></time>
     <?php endif; ?>
-    <?php if (!empty($endtime) && $context->isOngoing()): ?>
-        - <time class="dtend" datetime="<?php echo $endu->format('c') ?>"><?php echo $endu->format('M. j, Y')?></time>
+    <?php if (!empty($endtime) && $context->isOngoing()): ?>&ndash; <time class="dtend" datetime="<?php echo $endu->format('c') ?>"><?php echo $endu->format('M. j, Y')?></time>
     <?php endif; ?>
 </span>
 <span class="time-wrapper eventicon-clock">
     <?php if ($context->isAllDay()): ?>
     All Day
     <?php else: ?>
-        <?php echo $startu->format('g:i a')?><?php if (!empty($endtime) && $endtime != $starttime): ?>-<?php echo $endu->format('g:i a')?><?php endif; ?>
+        <?php echo $startu->format('g:i a')?><?php if (!empty($endtime) && $endtime != $starttime): ?>&ndash;<?php echo $endu->format('g:i a')?><?php endif; ?>
     <?php endif; ?>
-    
+
 </span>
