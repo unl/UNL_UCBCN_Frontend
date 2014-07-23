104836deeef0c48916de09f50937e3f32bc005e3
Kevin Abel <kevin.abel.0@gmail.com>
2014-07-23 15:24:06 -0500

    Add year navigation to year template and style

--- www/templates/default/html/Year.tpl.php
+++ www/templates/default/html/Year.tpl.php
@@ -1,4 +1,12 @@
-<h1 class="year_main"><?php echo $context->year; ?></h1>
+<?php 
+$lastYear = $context->getRawObject()->getDateTime()->modify('-1 year');
+$nextYear = $context->getRawObject()->getDateTime()->modify('+1 year');
+?>
+<div class="year-nav">
+    <a href="<?php echo UNL\UCBCN\Frontend\Year::generateURL($context->getRaw('calendar'), $lastYear) ?>"><span class="eventicon-angle-circled-left"></span><?php echo $lastYear->format('Y') ?></a>
+    <h1 class="year_main"><?php echo $context->year; ?></h1>
+    <a href="<?php echo UNL\UCBCN\Frontend\Year::generateURL($context->getRaw('calendar'), $nextYear) ?>"><?php echo $nextYear->format('Y') ?><span class="eventicon-angle-circled-right"></span></a>
+</div>
 <div class="year_cal wdn-grid-set bp1-wdn-grid-set-halves bp2-wdn-grid-set-thirds">
 <?php foreach ($context->getRaw('monthwidgets') as $widget): ?>
 <div class="wdn-col">
