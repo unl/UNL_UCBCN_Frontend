e47c969f1824af9dce360b1609d5e98bf012a71b
Ryan Dee <ryan@skoolbus39.com>
2014-07-24 10:08:57 -0500

    Reduce header size

--- www/templates/default/html/EventInstance/Summary.tpl.php
+++ www/templates/default/html/EventInstance/Summary.tpl.php
@@ -1,3 +1,3 @@
-<h2><a class="url summary" href="<?php echo $frontend->getEventURL($context->getRawObject()) ?>">
+<h3><a class="url summary" href="<?php echo $frontend->getEventURL($context->getRawObject()) ?>">
     <?php echo $savvy->dbStringtoHtml($context->event->title) ?>
-</a></h2>
+</a></h3>
