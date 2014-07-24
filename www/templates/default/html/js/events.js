4ee0441070c13f7deed13bc1becefbfcef2353d5
Ryan Dee <ryan@skoolbus39.com>
2014-07-24 11:46:18 -0500

    Style 'Return to today' link

--- www/templates/default/html/js/events.js
+++ www/templates/default/html/js/events.js
@@ -1,27 +1,27 @@
 require(['jquery', 'wdn'], function($, WDN) {
 	var $progress = $('<progress>'),
 		months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-	
+
 	$(function() {
 		var homeUrl = $('link[rel=home]'),
 			$monthWidget = $('.wp-calendar'),
 			widgetDate, nowActive, progressTimeout;
-		
+
 		if (homeUrl.length) {
 			homeUrl = homeUrl[0].href;
 		} else {
 			homeUrl = '/';
 		}
-		
+
 		function addMonthWidgetStates()
 		{
 			widgetDate = new Date($monthWidget.data('datetime'));
-			
+
 			var now = new Date(),
 				month = widgetDate.getMonth(),
 				year = widgetDate.getFullYear(),
 				$dates = $('td', $monthWidget);
-			
+
 			$dates.removeClass('today active');
 			if (year === now.getFullYear() && month === now.getMonth()) {
 				$dates.not('.prev,.next').each(function() {
@@ -32,7 +32,7 @@ require(['jquery', 'wdn'], function($, WDN) {
 					}
 				});
 			}
-			
+
 			if (year == nowActive.getFullYear() && month == nowActive.getMonth()) {
 				$dates.not('.prev,.next').each(function() {
 					var dateText = $.trim($(this).text());
@@ -43,7 +43,7 @@ require(['jquery', 'wdn'], function($, WDN) {
 				});
 			};
 		}
-		
+
 		function scheduleProgress($loadTo)
 		{
 			cancelProgress();
@@ -53,12 +53,12 @@ require(['jquery', 'wdn'], function($, WDN) {
 				$progress.fadeIn(200);
 			}, 1000)
 		}
-		
+
 		function cancelProgress()
 		{
 			clearTimeout(progressTimeout);
 		}
-		
+
 		function determineActiveDay()
 		{
 			var headingDate = $('h1').data('datetime');
@@ -68,16 +68,16 @@ require(['jquery', 'wdn'], function($, WDN) {
 				nowActive = new Date();
 			}
 		}
-		
+
 		function loadMonthWidget(datetime)
 		{
 			var url, $loadTo = $('aside .calendar');
 			if (datetime instanceof Date) {
-				url = homeUrl + datetime.getFullYear() + '/' + (datetime.getMonth() + 1) + '/'; 
+				url = homeUrl + datetime.getFullYear() + '/' + (datetime.getMonth() + 1) + '/';
 			} else {
 				url = datetime;
 			}
-			
+
 			scheduleProgress($loadTo);
 			$.get(url + '?monthwidget&format=hcalendar', function(data) {
 				cancelProgress();
@@ -86,7 +86,7 @@ require(['jquery', 'wdn'], function($, WDN) {
 				addMonthWidgetStates();
 			});
 		}
-		
+
 		function changeDay(datetime)
 		{
 			var url, $loadTo = $('#updatecontent');
@@ -95,7 +95,7 @@ require(['jquery', 'wdn'], function($, WDN) {
 			} else {
 				url = datetime;
 			}
-			
+
 			scheduleProgress($loadTo);
 			$.get(url + '?format=hcalendar', function(data) {
 				cancelProgress();
@@ -108,55 +108,55 @@ require(['jquery', 'wdn'], function($, WDN) {
 				}
 			});
 		}
-		
+
 		function loadEventInstance(href)
 		{
 			var $loadTo = $('#updatecontent');
-			
+
 			scheduleProgress()
 			$.get(href + '?format=hcalendar', function(data) {
 				cancelProgress();
 				$loadTo.html(data);
 			});
 		}
-		
+
 		var $sidebarCal = $('aside .calendar');
 		if ($sidebarCal.length) {
 			determineActiveDay();
 			addMonthWidgetStates();
-			
+
 			// Add a button for returning to "Today"
-			$('<p>', {'class': 'wdn-center'})
-				.append($('<a>', {'class': 'wdn-button', 'href': '#'}).text('Today'))
+			$('<p>')
+				.append($('<a>', {'class': 'return-today eventicon-angle-circled-left', 'href': '#'}).text('Return to today'))
 				.click(function(e) {
 					e.preventDefault();
 					changeDay(new Date());
 				})
 				.insertAfter($sidebarCal);
-			
+
 			$sidebarCal.on('click', 'td a', function(e) {
 				e.preventDefault();
 				changeDay(this.href);
 			});
-			
+
 			$sidebarCal.on('click', '.next a, .prev a', function(e) {
 				e.preventDefault();
 				loadMonthWidget(this.href);
 			})
 		}
-		
+
 		// set up arrow navigation
 		$(document).on('keyup', function(e) {
 			if ($(e.target).is('input, select, textarea, button')) {
 				return;
 			}
-			
+
 			var $dayNav = $('.day-nav');
-			
+
 			if (!$dayNav.length) {
 				return;
 			}
-			
+
 			switch (e.which) {
 				case 39:
 					changeDay($('.next', $dayNav).attr('href'));
@@ -166,7 +166,7 @@ require(['jquery', 'wdn'], function($, WDN) {
 					break;
 			}
 		});
-		
+
 		$('#updatecontent').on('click', '.vevent a.summary', function(e) {
 			e.preventDefault();
 			loadEventInstance(this.href);
