2aa79393f9864188dd40ba992e6eaba53ceb960b
Kevin Abel <kevin.abel.0@gmail.com>
2014-08-01 10:55:03 -0500

    HTML validation issue in search template
    Use ampersands for format and remove excess closing span.

--- www/templates/default/html/Search.tpl.php
+++ www/templates/default/html/Search.tpl.php
@@ -3,17 +3,17 @@
         <h1 class="results clear-top">
             <?php
             if ($dt = $context->getSearchTimestamp()) {
-                echo '<span class="wdn-subhead">'.$context->count().' search results for events dated </span><a class="permalink" href="'.$context->getURL().'">'.date('F jS',$dt).'</a></span>';
+                echo '<span class="wdn-subhead">'.$context->count().' search results for events dated </span><a class="permalink" href="'.$context->getURL().'">'.date('F jS',$dt).'</a>';
             } else {
-                echo '<span class="wdn-subhead">'.$context->count().' search results for </span><a class="permalink" href="'.$context->getURL().'">'.htmlentities($context->search_query).'</a></span>';
+                echo '<span class="wdn-subhead">'.$context->count().' search results for </span><a class="permalink" href="'.$context->getURL().'">'.htmlentities($context->search_query).'</a>';
             }
             ?>
         </h1>
         <div id="subscribe">
             <span>Subscribe to this search</span>
             <ul id="droplist">
-                <li id="eventrss"><a href="<?php echo $context->getURL()?>?format=rss" class="eventicon-rss">RSS</a></li>
-                <li id="eventical"><a href="<?php echo $context->getURL()?>?format=ics" class="wdn-icon-calendar">ICS</a></li>
+                <li id="eventrss"><a href="<?php echo $context->getURL()?>&amp;format=rss" class="eventicon-rss">RSS</a></li>
+                <li id="eventical"><a href="<?php echo $context->getURL()?>&amp;format=ics" class="wdn-icon-calendar">ICS</a></li>
             </ul>
         </div>
     </section>
